1df2eb80943c70ebf2534a06c5d2638795973a8b
Harikrishnan N <harikrishnan.n@edifydata.com>
2021-06-24 08:58:39 +0530

    search icons in pages

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,10 +1,11 @@
+import Icon from "@material-ui/core/Icon";
 
 export default function Blogs() {
     return (
         <>
              <div className="flex flex-row justify-end mr-16 mt-8">
                 <span className="font-bold mr-5">21 June, 2021</span>
-                <span className="material-icons-outlined">search</span>
+                <Icon color="primary">search</Icon>
             </div>
             <div className="flex  justify-center mt-10">
                 <div className="h-44 w-4/5 flex flex-col justify-center rounded-3xl bg-pink-200">
