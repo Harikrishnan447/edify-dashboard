0962a4b98f4ad1bbbfebdec2a9e98e52cd43dced
Harikrishnan N <harikrishnan.n@edifydata.com>
2021-06-25 18:36:38 +0530

    data table added.

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,17 +1,127 @@
-import Icon from "@material-ui/core/Icon";
+import DataTable from "react-data-table-component";
+import Card from "@material-ui/core/Card";
+import SortIcon from "@material-ui/icons/ArrowDownward";
 
-export default function Dashboard() {
+const columns = [
+    {
+      name: "Sl.No",
+      selector: "slno",
+      sortable: true,
+      cell: (movies:any) => 
+        <div style={
+          { fontWeight : 500,
+            fontFamily : 'poppins',
+            fontSize   :   'large'  
+          }
+        }>{movies.slno}</div>,
+    },
+    {
+      name: "Title",
+      selector: "title",
+      sortable: true,
+      cell: (movies:any) => 
+      <div style={
+        { fontWeight  : 500,
+          fontFamily  : 'poppins',
+          fontSize    :'large'  
+        }
+      }>{movies.title}</div>,
+    },
+    {
+      name: "Date",
+      selector: "date",
+      sortable: true,
+      right: false,
+      cell: (movies:any) => 
+      <div style={
+        { fontWeight  : 500,
+          fontFamily  : 'poppins',
+          fontSize    :'large' 
+        }
+      }>{movies.date}</div>,
+    }
+];
+
+const movies = [
+    {
+        slno: 1,
+        title: "New Year",
+        date: "January 1, 2021",
+    },
+    {
+      slno: 2,
+      title: "Republic Day",
+      date: "January 26, 2021",
+    },
+    {
+      slno: 3,
+      title: "Shivarathri",
+      date: "March 11, 2021",
+    },
+    {
+      slno: 4,
+      title: "Good Friday",
+      date: "April 2, 2021",
+    },
+    {
+      slno: 5,
+      title: "Vishu",
+      date: "April 14, 2021",
+    },
+    {
+      slno: 6,
+      title: "May Day",
+      date: "May 1, 2021",
+    },
+    {
+      slno: 7,
+      title: "Ramzan",
+      date: "May 13, 2021",
+    },{
+      slno: 8,
+      title: "Onam",
+      date: "August 20, 2021",
+    },{
+      slno: 9,
+      title: "Christmas",
+      date: "December 25, 2021",
+    },
+]
+const customStyles = {
+  
+  headCells: {
+    style: {
+      fontSize: '25px',
+      fontWeight: '500',
+    },
+  },
+  rows: {
+    style: {
+      minHeight: '52px', // override the row height
+    }
+  },
+  cells: {
+    style: {
+      paddingLeft: '8px', // override the cell padding for data cells
+      paddingRight: '8px',
+    },
+  },
+};
+
+export default function Events() {
     return (
-        <>
-             <div className="flex flex-row justify-end mr-16 mt-8">
-                  <span className="font-bold mr-5">21 June, 2021</span>
-                  <Icon color="primary">search</Icon>
-                </div>
-                <div className="flex  justify-center mt-10">
-                  <div className="h-44 w-4/5 flex flex-col justify-center rounded-3xl bg-pink-200">
-                      <span className="ml-10 font-bold text-3xl text-pink-400">Holidays and Events....!</span>
-                  </div>
-            </div>
-        </>
-    );
+        <div className="mt-20 py-5 px-8">
+          <Card>
+            <DataTable
+              columns={columns}
+              data={movies}
+              defaultSortField="title"
+              sortIcon={<SortIcon />}
+              pagination
+              selectableRows
+              customStyles={customStyles}
+            />
+          </Card>
+        </div>
+      );
 }
--- src/pages/Leave.tsx
+++ src/pages/Leave.tsx
@@ -1,15 +1,15 @@
 import Icon from "@material-ui/core/Icon";
 
-export default function Dashboard() {
+export default function Leave() {
     return (
         <>
              <div className="flex flex-row justify-end mr-16 mt-8">
                   <span className="font-bold mr-5">21 June, 2021</span>
                   <Icon color="primary">search</Icon>
-                </div>
-                <div className="flex  justify-center mt-10">
+              </div>
+              <div className="flex  justify-center mt-10">
                   <div className="h-44 w-4/5 flex flex-col justify-center rounded-3xl bg-pink-200">
-                      <span className="ml-10 font-bold text-3xl text-pink-400">Leave Requests ....!</span>
+                      <span className="ml-10 font-bold text-3xl text-pink-400">Leave Requests...?</span>
                   </div>
             </div>
         </>
