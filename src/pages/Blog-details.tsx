8e05c150f06a0b87eaad90801cc65ec362929732
Harikrishnan N <harikrishnan.n@edifydata.com>
2021-06-29 15:48:14 +0530

    sidebar for blog entry.

--- src/pages/Blog-details.tsx
+++ src/pages/Blog-details.tsx
@@ -6,31 +6,30 @@ export default function BlogDetails() {
            <div className="px-3 md:px-2 font-poppins">
            <div className="w-full rounded bg-white px-6 py-6">
            <div className="font-semibold tracking-wide text-xl mb-2 gap-4 flex justify-between items-center">
-               <div>
+               <div className="flex gap-8 items-center">
                <span>Tailwind CSS V2.2</span>
                <span className="font-normal text-sm">Friday, 01 June 2021</span>
                </div>
                <div className="inline-flex rounded border-solid border-2 border-grey-300">
-  <button className=" text-sm  text-primary  py-2 px-4 " title="Delete this article">
-  <Icon>delete</Icon>
-  </button>
-  <button className="text-sm  py-2 px-4 " title="Edit this article">
-  <Icon>edit</Icon>
-  </button>
-  <button className="text-sm  py-2 px-4" title="Approve">
-  <Icon>check</Icon>
-  </button>
-</div>
-               
-               </div>
-               <div>
-                   <span className="font-normal text-sm text-grey-200">
-                       Written by:
-                   </span>
-                   <span className="font-normal text-base text-black-200 ml-2">
-                       Jose Mathew
-                   </span>
-               </div>
+                  <button className=" text-sm  text-primary  py-2 px-4 " title="Delete this article">
+                  <Icon>delete</Icon>
+                  </button>
+                  <button className="text-sm  py-2 px-4 " title="Edit this article">
+                  <Icon>edit</Icon>
+                  </button>
+                  <button className="text-sm  py-2 px-4" title="Approve">
+                  <Icon>check</Icon>
+                  </button>
+              </div>
+            </div>
+            <div>
+                <span className="font-normal text-sm text-grey-200">
+                    Written by:
+                </span>
+                <span className="font-normal text-base text-black-200 ml-2">
+                    Jose Mathew
+                </span>
+            </div>
       <img className="rounded mt-5 mb-4"  src="https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg" alt="Mountain"/>
       <div className="">
         <p className="text-gray-500 text-base mt-3 tracking-wide">
--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,6 +1,7 @@
 import Icon from "@material-ui/core/Icon";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import ImageUploader from 'react-images-upload';
 
 const blogs = [
     {
@@ -21,8 +22,21 @@ const blogs = [
 ]
 
 export default function Blogs() {
+    const [showSidebar, setShowSidebar] = useState("-left-80");
+
+    function onDrop(picture : any) {
+        console.log(picture);
+    }
+
     return (
         <>
+        <div className="flex justify-end px-4 md:px-12">
+            <button className="text-white text-base"
+                onClick={() => setShowSidebar('right-0')}
+            >
+                <Icon color="primary">edit</Icon>
+            </button>
+        </div>
         <div className="px-3 md:px-2">
             <div className="grid md:grid-flow-col md:grid-cols-3 gap-4 font-poppins">
                 {blogs.map((blog, index) => (
@@ -46,6 +60,43 @@ export default function Blogs() {
                 ))}
             </div>
         </div>
+        <div
+                className={`h-screen fixed top-0 md:right-0 ${showSidebar} flex-row flex-nowrap shadow-xl bg-white w-80 z-10 py-4 px-6 transition-all duration-300`}
+            >
+                <div className="flex-col items-stretch min-h-full flex-nowrap pt-4 relative">
+                    <div className="flex justify-end">
+                        <button className=""  onClick={() => setShowSidebar('-left-80')} >
+                            <Icon color="primary">close</Icon>
+                        </button>
+                    </div>
+                    <div className="mt-10">
+                        <span className="text-lg font-bold font-poppins">Create a blog</span>
+                    </div>
+                    <div className="flex flex-col mt-8">
+                        <label className="block text-gray-700 text-sm font-semibold font-poppins mb-2">
+                            Username
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Add a title"></input>
+                        <label className="block text-gray-700 text-sm font-semibold font-poppins mb-2 mt-4">
+                            Cover Image
+                        </label>
+                        <ImageUploader
+                            withIcon={true}
+                            buttonText='Choose images'
+                            onChange={onDrop}
+                            maxFileSize={5242880}
+                            accept="jpg/ gif/ png"
+                        />
+                        <label className="block text-gray-700 text-sm font-semibold font-poppins mb-2 mt-4">
+                            Content
+                        </label>
+                        <textarea className="resize h-32 rounded-md shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Add a content.."></textarea>
+                        <button className="w-1/2 bg-red-400 hover:bg-red-600 text-white font-semibold font-poppins mt-10 py-2 px-4 rounded-full">
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
