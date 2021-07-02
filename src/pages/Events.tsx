6f764e1b25c3ae4a1f8616f01e37359a5b12c263
Harikrishnan N <harikrishnan.n@edifydata.com>
2021-07-02 14:29:45 +0530

    sidebar added for adding new events in 'events' page.

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import DataTable from "react-data-table-component";
 import Card from "@material-ui/core/Card";
 import SortIcon from "@material-ui/icons/ArrowDownward";
+import Icon from "@material-ui/core/Icon"
 
 const columns = [
     {
@@ -109,8 +111,18 @@ const customStyles = {
 };
 
 export default function Events() {
+    const [showSidebar, setShowSidebar] = useState("-left-80 md:-left-96");
+
     return (
-        <div className="mt-6 py-5 px-8">
+      <>
+        <div className="flex justify-end px-4 md:px-12">
+            <button className="text-white text-base"
+                onClick={() => setShowSidebar('right-0')}
+            >
+                <Icon color="primary">edit</Icon>
+            </button>
+        </div>
+        <div className="mt-3 py-2 px-3">
           <Card>
             <DataTable
               columns={columns}
@@ -123,5 +135,37 @@ export default function Events() {
             />
           </Card>
         </div>
+        <div
+            className={`h-screen fixed top-0 md:right-0 ${showSidebar} flex-row flex-nowrap shadow-xl bg-white w-80 md:w-96 z-10 py-4 px-6 transition-all duration-300`}
+        >
+            <div className="flex-col items-stretch min-h-full flex-nowrap pt-4 relative">
+                <div className="flex justify-end">
+                    <button className=""  onClick={() => setShowSidebar('-left-80 md:-left-96')} >
+                        <Icon color="primary">close</Icon>
+                    </button>
+                </div>
+                <div className="mt-4">
+                    <span className="text-lg font-bold font-poppins">Add an employee</span>
+                </div>
+                <div className="flex flex-col mt-8">
+                    <label className="block text-gray-700 text-sm font-semibold font-poppins mb-2">
+                        Title
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" type="text" placeholder="Title"></input>
+                    <label className="block text-gray-700 text-sm font-semibold font-poppins mt-6 mb-2">
+                        Date
+                    </label>
+                    <input type="date" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="date" placeholder="Date"></input>
+                    <label className="block text-gray-700 text-sm font-semibold font-poppins mt-6 mb-2">
+                        Please choose a category
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="category" type="text" placeholder="Please choose a category"></input>
+                    <button className="w-1/2 bg-red-400 hover:bg-red-600 text-white font-semibold font-poppins mt-10 py-2 px-4 rounded-full">
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </div>
+      </>
       );
 }
